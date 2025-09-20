1705aa8334db69db1a5c0d7c0c1737a3a4100e26
abcd3541 <yifei.zhu1221@gmail.com>
2025-09-20 10:30:32 +0800

    Slight fixes and edits

--- Script_Gui.py
+++ Script_Gui.py
@@ -2,10 +2,12 @@ import customtkinter as tk
 import time
 import os
 import threading
+import pygame as pg
 from Roblox_Search_Script import main
 cookie = None
 path = "C:/ServerFinder/"
 running = False
+sound = "Ding.mp3"
 
 def output(text):
     timestamp = time.strftime("%H:%M:%S")
@@ -40,6 +42,7 @@ def save_cookie():
 def run_search_script():
     try:
         output(main(cookie, PlaceID))
+        play_sound(sound)
     except Exception as e:
         output(f"Failed to run search script because of {e}")
 
@@ -59,17 +62,41 @@ def Gui_main():
         return
     try:
         if not running:
-            PlaceID_textbox.configure(state="disabled")
             running = True
             output("Searching for game Servers in HK and SG")
             start_search_thread()
             running = False
-            PlaceID_textbox.configure(state="enabled")
+
         else:
             output(f"Already running for {PlaceID}")
     except Exception as e:
         print(e)
 
+def play_sound(sound_effect):
+    try:
+        sound = pg.mixer.Sound(sound_effect)
+        sound.play()
+    except Exception as e:
+        print(f"Error playing sound {sound_effect}: {e}")
+
+def choose_sound():
+    global sound
+    sound_in = tk.CTkInputDialog(text="Input Name of Sound File:", title="Sound File")
+    extract = sound_in.get_input()
+    sound = extract.strip()
+    if not sound:
+        output("Sound file not entered.")
+        return
+    try:
+        os.makedirs(path, exist_ok=True)
+        if os.path.exists(path):
+            output(f"Directory exists")
+            if os.path.exists(sound):
+                output(f"Found sound file: '{sound}'")
+
+    except Exception as e:
+        output(f"Failed to create directory '{path}'")
+        return
 
 
 def load_cookie():
@@ -85,38 +112,46 @@ def load_cookie():
         output(f"Failed to load cookie because of '{e}'")
 
 
+if __name__ == "__main__":
+
+    root = tk.CTk()
+    root.geometry('500x600')
+    root.title('funky functions')
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+    pg.mixer.init()
+
+    frame = tk.CTkFrame(root,width=400,height=500)
+    frame.pack(pady=30)
+
+    title_label =  tk.CTkLabel(frame,text='Roblox Server Search',font=tk.CTkFont(size=30,weight='bold'))
+    title_label.pack(padx=10,pady=(30,10))
+
+    label1 =  tk.CTkLabel(frame,text='Roblox Game ID',font=tk.CTkFont(size=20))
+    label1.pack(padx=10,pady=(20, 10))
+
+    PlaceID_textbox = tk.CTkTextbox(frame,height=25,width=400,font=tk.CTkFont(size=15),activate_scrollbars=False)
+    PlaceID_textbox.pack(padx=10,pady=(5,10))
+
+    label2 =  tk.CTkLabel(frame,text='Output',font=tk.CTkFont(size=20))
+    label2.pack(padx=10,pady=(10, 10))
+
+    Out_textbox = tk.CTkTextbox(frame,height=200,width=400,font=tk.CTkFont(size=15),state="disabled")
+    Out_textbox.pack(padx=10,pady=(5,10))
+
+    run_button = tk.CTkButton(frame,text='Run',command=Gui_main)
+    run_button.pack(pady=5)
+
+    cookie_button = tk.CTkButton(root, text="Save Cookies", command=save_cookie,height=5,width=15,corner_radius=3)
+    cookie_button.pack(side=tk.BOTTOM, anchor=tk.SE)
+
+    cookie_load_button = tk.CTkButton(root, text="Load Cookies", command=load_cookie,height=5,width=15,corner_radius=3)
+    cookie_load_button.pack(pady=(5,5) ,side=tk.BOTTOM, anchor=tk.SE)
+
+    sound_button = tk.CTkButton(root, text="Change Sound", command=choose_sound,height=10,width=15)
+    sound_button.pack(side=tk.BOTTOM, anchor=tk.SW)
 
 
-root = tk.CTk()
-root.geometry('500x600')
-root.title('funky functions')
 
-title_label =  tk.CTkLabel(root,text='Roblox Server Search',font=tk.CTkFont(size=30,weight='bold'))
-title_label.pack(padx=10,pady=(30,10))
-
-label1 =  tk.CTkLabel(root,text='Roblox Game ID',font=tk.CTkFont(size=20))
-label1.pack(padx=10,pady=(20, 10))
-
-PlaceID_textbox = tk.CTkTextbox(root,height=25,width=400,font=tk.CTkFont(size=15),activate_scrollbars=False)
-PlaceID_textbox.pack(padx=10,pady=(10,10))
-
-label2 =  tk.CTkLabel(root,text='Output',font=tk.CTkFont(size=20))
-label2.pack(padx=10,pady=(10, 10))
-
-Out_textbox = tk.CTkTextbox(root,height=200,width=400,font=tk.CTkFont(size=15),state="disabled")
-Out_textbox.pack(padx=10,pady=(10,10))
-
-run_button = tk.CTkButton(root,text='Run',command=Gui_main)
-run_button.pack()
-
-cookie_button = tk.CTkButton(root, text="Save Cookies", command=save_cookie,height=5,width=15,corner_radius=3)
-cookie_button.pack(padx=10, pady=5, side=tk.BOTTOM, anchor=tk.SE)
-
-cookie_load_button = tk.CTkButton(root, text="Load Cookies", command=load_cookie,height=5,width=15,corner_radius=3)
-cookie_load_button.pack(padx=10, pady=5, side=tk.BOTTOM, anchor=tk.SE)
-
-
-
-
-output("Innitializing...")
-root.mainloop()
+    output("Innitializing...")
+    root.mainloop()
